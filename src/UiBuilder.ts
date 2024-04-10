e76da0f17fff2397aad4768ce0cf7ee5a4a1f32e
Walker Aldridge <daldridge@lairdscomputer.com>
2024-04-10 15:52:36 -0500

    Change to passing event

--- src/UiBuilder.ts
+++ src/UiBuilder.ts
@@ -37,11 +37,11 @@ export class UiBuilder {
         a.id = "enhanced_muted_words";
         a.setAttribute("style", "padding-right: 16px; padding-left: 16px;");
         a.innerHTML = `<div class="css-175oi2r r-1awozwy r-18u37iz r-16y2uox"><div class="css-175oi2r r-16y2uox r-1wbh5a2"><div dir="ltr" class="css-1rynq56 r-bcqeeo r-qvutc0 r-37j5jr r-a023e6 r-rjixqe r-16dba41" style="text-overflow: unset; color: rgb(231, 233, 234);"><span class="css-1qaijid r-bcqeeo r-qvutc0 r-poiln3" style="text-overflow: unset;">Enhanced muted words</span></div></div><svg viewBox="0 0 24 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1bwzh9t r-1q142lx r-f727ji"><g><path d="M14.586 12L7.543 4.96l1.414-1.42L17.414 12l-8.457 8.46-1.414-1.42L14.586 12z"></path></g></svg></div>`;
-        a.addEventListener("mouseenter", function () {
-            this.classList.add("r-g2wdr4");
+        a.addEventListener("mouseenter", evt => {
+            (evt.target as HTMLElement).classList.add("r-g2wdr4");
         });
-        a.addEventListener("mouseleave", function () {
-            this.classList.remove("r-g2wdr4");
+        a.addEventListener("mouseleave", evt => {
+            (evt.target as HTMLElement).classList.remove("r-g2wdr4");
         });
         return a;
     }
